d68de921390709df716f084adb82928c7f2aef53
puts hola <ftoapanta@hotmail.com>
2018-09-23 17:50:52 -0500

    Implementacion de viewmodel y los binding

---
+++ Estudio1/Estudio1/Models/Cotizacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estudio1.Models
+{
+    public class Cotizacion
+    {
+        public int Id { get; set; }
+        public string Moneda { get; set; }
+        public decimal Tasa { get; set; }
+        public string Nombre { get; set; }
+    }
+}
--- Estudio1/Estudio1/ViewModels/MainViewModel.cs
+++ Estudio1/Estudio1/ViewModels/MainViewModel.cs
@@ -1,6 +1,38 @@
-﻿namespace Estudio1.ViewModels
+﻿using Estudio1.Models;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows.Input;
+
+namespace Estudio1.ViewModels
 {
     public class MainViewModel
     {
+        #region Propiedades
+        public string Monto { get; set; }
+        public ObservableCollection<Cotizacion> Cotizaciones { get; set; }
+        public Cotizacion CotizacionOrigen { get; set; }
+        public Cotizacion CotizacionDestino { get; set; }
+        public bool EstaCorriendo { get; set; }
+        public bool EstaHabilitado { get; set; }
+        public string Resultado { get; set; }
+
+        #endregion
+
+        #region Comandos
+        public ICommand MyProperty {
+            get
+            {
+                return new RelayCommand(Convertir);
+            }
+        }
+
+        private void Convertir()
+        {
+            Resultado = "Listo";
+            //throw new NotImplementedException();
+        }
+        #endregion
     }
 }
