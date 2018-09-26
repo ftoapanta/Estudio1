e5dd7a12b3badd2cd6f0fa0fe1f1ceb7d5361bce
puts hola <ftoapanta@hotmail.com>
2018-09-25 23:39:09 -0500

    activar binding prpiedad eventChange

--- Estudio1/Estudio1/ViewModels/MainViewModel.cs
+++ Estudio1/Estudio1/ViewModels/MainViewModel.cs
@@ -2,31 +2,69 @@
 using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight.Command;
 using System.Windows.Input;
+using Xamarin.Forms;
+using System.ComponentModel;
 
 namespace Estudio1.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        //para que se active el binding
+        public event PropertyChangedEventHandler PropertyChanged;
+        #region Atributos Para ue se ejecute los binding
+        bool estaCorriendo { get; set; }
+        string resultado { get; set; }
+        #endregion
+
         #region Propiedades
+        public bool EstaCorriendo
+        {
+            get
+            {
+                return estaCorriendo;
+            }
+            set
+            {
+                if (estaCorriendo != value)
+                {
+                    estaCorriendo = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EstaCorriendo)));
+                }
+
+            }
+        }
+        public string Resultado
+        {
+            get
+            {
+                return resultado;
+            }
+            set
+            {
+                if (resultado != value)
+                {
+                    resultado = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Resultado)));
+                }
+
+            }
+        }
         public string Monto { get; set; }
         public ObservableCollection<Cotizacion> Cotizaciones { get; set; }
         public Cotizacion CotizacionOrigen { get; set; }
         public Cotizacion CotizacionDestino { get; set; }
-        public bool EstaCorriendo { get; set; }
         public bool EstaHabilitado { get; set; }
-        public string Resultado { get; set; }
 
         #endregion
 
         #region Comandos
-        public ICommand ConvertirCommand {
+        public ICommand ConvertirCommand { 
             get
             {
                 return new RelayCommand(Convertir);
             }
         }
-
-        public void Convertir()
+        void Convertir()
         {
             Resultado = "Listo";
             //throw new NotImplementedException();
@@ -35,9 +73,13 @@ namespace Estudio1.ViewModels
 
         public MainViewModel()
         {
+            CargarCotizaciones();
+        }
+
+        void CargarCotizaciones()
+        {
             EstaCorriendo = true;
-            EstaHabilitado = true;
-            Resultado = "......";
+            Resultado = "Cargando monedas...";
         }
     }
 }
